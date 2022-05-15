4da45e7baf3058b177343ac8c2ba4fb2e6cbf168
phancaovu <phanvu2000ht@gmail.com>
2022-05-15 18:48:42 +0700

    them , xoa , sua ,list ,login, logout jwt

---
+++ router/User.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const AuthController = require('../controllers/AuthController')
+const userController = require('../controllers/userController')
+
+router.get("/", userController.index)
+router.post("/them", userController.insert)
+router.post("/show", userController.show)
+router.post("/sua", userController.Update)
+router.post("/xoa", userController.del)
+router.post("/register", AuthController.register)
+router.post("/login", AuthController.login)
+
+
+module.exports = router
